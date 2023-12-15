dc83e85ad26fdf753f147000d7801de5ce8f5f79
Mfrsrzdnsy <muhammadfiras2602@gmail.com>
2023-12-15 16:07:52 +0800

    percobaan login multiuser

---
+++ app/Http/Controllers/AgenController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AgenController extends Controller
+{
+    public function AgenDashboard(){
+        
+        return view('agen.agen_dashboard');
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AgenController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +31,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+Route::get('/agen/dashboard', [AgenController::class, 'AgenDashboard'])->name('agen.dashboard');
