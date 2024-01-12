2317d38d1d36d1fc51bcd639b548e0620fa929c5
Mfrsrzdnsy <muhammadfiras2602@gmail.com>
2024-01-12 14:39:17 +0800

    update laporan anak

--- resources/views/laporan_siswa.blade.php
+++ resources/views/laporan_siswa.blade.php
@@ -1,70 +1,82 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>download PDF</title>
-	<style>
-		table, th, td {
-		  border:1px solid black;
-		  border-collapse: collapse;
-		}
-		</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Data Anak Putus Sekolah - PDF</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
 
+        table {
+            border-collapse: collapse;
+            width: 100%;
+            margin-bottom: 20px;
+        }
+
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        h2 {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        img {
+            max-width: 100%;
+            height: auto;
+        }
+    </style>
 </head>
 <body>
-	
-	<div class="page-content">
 
-        <div class="row">
-            <div class="col-md-12 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body">
-                        <h2 class="text-center">Data Anak Putus Sekolah</h2>
-                        <div class="table-responsive">
-                            <table id="dataTableExample" class="table" style="width:100%">
-								<thead>
-									<tr>
-										<th>NIS</th>
-										<th>Nama</th>
-										<th>Jenis Kelamin</th>
-										<th>Tempat Lahir</th>
-										<th>Tanggal Lahir</th>
-										<th>Alamat</th>
-										<th>Pendidikan</th>
-										<th>Deskripsi</th>
-									</tr>
-								</thead>
-									<tbody>
-										@foreach ($siswas as $siswa)
-                                    <tr style="height:200px">
-                                       
-										<td>{{ $siswa->nis }}</td>
-                                        
-										<td>{{ $siswa->nama }}</td>
-                                        
-										<td>{{ $siswa->jenis_kelamin }}</td>
-                                        
-										<td>{{ $siswa->tempat_lahir }}</td>
-                                        
-										<td>{{ $siswa->tanggal_lahir }}</td>
-                                        
-										<td>{{ $siswa->alamat }}</td>
-										
-										<td>{{ $siswa->pendidikan_terakhir }}
-										
-										<td>{{ $siswa->deskripsi }}</td>
-                                    </tr>
-									
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <div class="page-content">
+
+        <h2>Data Anak Putus Sekolah</h2>
+
+        <div class="table-responsive">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Foto</th>
+                        <th>NIS</th>
+                        <th>Nama</th>
+                        <th>Jenis Kelamin</th>
+                        <th>Tempat Lahir</th>
+                        <th>Tanggal Lahir</th>
+                        <th>Alamat</th>
+                        <th>Pendidikan</th>
+                        <th>Deskripsi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($siswas as $siswa)
+                    <tr>
+                        <td><img src="{{ asset('storage/' . $siswa->foto) }}" alt=""></td>
+                        <td>{{ $siswa->nis }}</td>
+                        <td>{{ $siswa->nama }}</td>
+                        <td>{{ $siswa->jenis_kelamin }}</td>
+                        <td>{{ $siswa->tempat_lahir }}</td>
+                        <td>{{ $siswa->tanggal_lahir }}</td>
+                        <td>{{ $siswa->alamat }}</td>
+                        <td>{{ $siswa->pendidikan_terakhir }}</td>
+                        <td>{{ $siswa->deskripsi }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
+
     </div>
+
 </body>
 </html>
